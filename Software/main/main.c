617ff53178279db1ffdf4bd9edc35ea628fd6636
Milos <milosnedeljkovic26@gmail.com>
2026-05-27 20:08:03 +0200

    PWM thrust for all motors.

--- Software/main/main.c
+++ Software/main/main.c
@@ -15,11 +15,14 @@
 
 
 #define MPU6050_REG_ACCEL_XOUT_H    0x3B        /*!< MPU6050 register address of accelerometer X high byte */
-#define GPIO_INPUT_PIN              GPIO_NUM_4  /*!< GPIO pin for input from MPU6050 sensor*/
-#define MOTOR1_CONTROL_PIN          GPIO_NUM_16 /*!< GPIO pin for motor 1 control (thrust) */
+#define GPIO_INPUT_PIN              GPIO_NUM_17  /*!< GPIO pin for input from MPU6050 sensor*/
+#define MOTOR1_CONTROL_PIN          GPIO_NUM_12 /*!< GPIO pin for motor 1 control */
+#define MOTOR2_CONTROL_PIN          GPIO_NUM_13 /*!< GPIO pin for motor 2 control */
+#define MOTOR3_CONTROL_PIN          GPIO_NUM_14 /*!< GPIO pin for motor 3 control */
+#define MOTOR4_CONTROL_PIN          GPIO_NUM_15 /*!< GPIO pin for motor 4 controlS */
 #define MPU6050_SENSITIVITY_2G      16384       /*!< MPU6050 sensitivity at ±2g full scale (LSB/g) */
 #define MPU6050_SENSITIVITY_250DPS   131        /*!< MPU6050 sensitivity at ±250°/s full scale (LSB/°/s) */
-#define MAX_THURST_VALUE            58500     /*!< Maximum thrust value for motor control is 65535 */
+#define MAX_THURST_VALUE            65535     /*!< Maximum thrust value for motor control is 65535 */
 // #define THURST_VALUE_90             58500      /*!< Maximum thrust value to avoid overloading motors */
 #define MAX_PWM_DUTY_CYCLE          8191        /*!< Maximum PWM duty cycle for 10-bit resolution */
 
@@ -44,6 +47,10 @@ typedef struct __attribute__((packed)) {
     uint8_t reserved;
 } crtp_packet_t;
 
+crtp_packet_t pkt;
+
+uint8_t rx_buffer[1024];
+int len;
 static mpu6050_regs mpu6050_data;
 static TaskHandle_t mpu6050_task_handle = NULL;
 
@@ -54,6 +61,12 @@ static void IRAM_ATTR mpu6050_isr_handler(void* arg)
     portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
 }
 
+const struct {ledc_channel_t ch; int gpio;} motors[] = {
+    {LEDC_CHANNEL_0, MOTOR1_CONTROL_PIN},
+    {LEDC_CHANNEL_1, MOTOR2_CONTROL_PIN},
+    {LEDC_CHANNEL_2, MOTOR3_CONTROL_PIN},
+    {LEDC_CHANNEL_3, MOTOR4_CONTROL_PIN},
+};
 static void timer_config(void)
 {
     ledc_timer_config_t ledc_timer = {
@@ -68,35 +81,23 @@ static void timer_config(void)
 
     printf("LEDC timer configured for motor control.\n");
 
-    ledc_channel_config_t ledc_channel = {
-        .speed_mode     = LEDC_HIGH_SPEED_MODE,
-        .channel        = LEDC_CHANNEL_0,
-        .timer_sel      = LEDC_TIMER_0,
-        .intr_type      = LEDC_INTR_DISABLE,
-        .gpio_num       = MOTOR1_CONTROL_PIN,
-        .duty           = 0, // Start with 0% duty cycle
-        .hpoint         = 0
-    };
+    for(int i = 0; i < 4; i++) {
+        ledc_channel_config_t ledc_channel = {
+            .speed_mode     = LEDC_HIGH_SPEED_MODE,
+            .channel        = motors[i].ch,
+            .timer_sel      = LEDC_TIMER_0,
+            .intr_type      = LEDC_INTR_DISABLE,
+            .gpio_num       = motors[i].gpio,
+            .duty           = 0, // Start with 0% duty cycle
+            .hpoint         = 0
+        };
 
-    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
-
+        ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+    }
+   
     printf("LEDC channel configured for motor control.\n");
 
 
-}
-
-static void motor_control(uint16_t thrust)
-{
-    if (thrust > MAX_THURST_VALUE) {
-        thrust = MAX_THURST_VALUE;
-    }
-
-    // thrust = (thrust / THURST_VALUE_90) * MAX_THURST_VALUE;
-    // Map thrust (0 to MAX_THURST_VALUE) to duty cycle (0 to 8191 for 13-bit resolution)
-    uint32_t duty = ((uint32_t)thrust * MAX_PWM_DUTY_CYCLE) / MAX_THURST_VALUE;
-    printf("Setting motor duty cycle to %ld for thrust %d\n", duty, thrust);
-    ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, duty));
-    ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0));
 }
 
 static void gpio_init(void)
@@ -162,11 +163,28 @@ static void vTask_mpu6050 (void * pvParametars)
         mpu6050_data.gyro_z  = (data[12] << 8) | data[13];
         
         printf("Accel: X=%.2f g Y=%.2f g Z=%.2f g | Gyro: X=%.2f o/s Y=%.2f o/s Z=%.2f o/s \n",
-                 (float)mpu6050_data.accel_x/MPU6050_SENSITIVITY_2G, (float)mpu6050_data.accel_y/MPU6050_SENSITIVITY_2G, (float)mpu6050_data.accel_z/MPU6050_SENSITIVITY_2G, 
-                 (float)mpu6050_data.gyro_x/MPU6050_SENSITIVITY_250DPS, (float)mpu6050_data.gyro_y/MPU6050_SENSITIVITY_250DPS, (float)mpu6050_data.gyro_z/MPU6050_SENSITIVITY_250DPS);
+                 (float)mpu6050_data.accel_x/MPU6050_SENSITIVITY_2G, 
+                 (float)mpu6050_data.accel_y/MPU6050_SENSITIVITY_2G, 
+                 (float)mpu6050_data.accel_z/MPU6050_SENSITIVITY_2G, 
+                 (float)mpu6050_data.gyro_x/MPU6050_SENSITIVITY_250DPS, 
+                 (float)mpu6050_data.gyro_y/MPU6050_SENSITIVITY_250DPS, 
+                 (float)mpu6050_data.gyro_z/MPU6050_SENSITIVITY_250DPS);
 
         // vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
+}
+
+static void motor_thrust(uint16_t thrust, uint8_t motor_index)
+{
+    if (thrust > MAX_THURST_VALUE) {
+        thrust = MAX_THURST_VALUE;
+    }
+
+    uint32_t duty = ((uint32_t)thrust * MAX_PWM_DUTY_CYCLE) / MAX_THURST_VALUE;
+    printf("Setting motor duty cycle to %ld for thrust %d\n", duty, thrust);
+    ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, motors[motor_index].ch, duty));
+    ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, motors[motor_index].ch));
+
 }
 
 static void parse_crtp_packet(uint8_t *data, ssize_t len)
@@ -174,20 +192,30 @@ static void parse_crtp_packet(uint8_t *data, ssize_t len)
     if (len < 2) {
         return; // Packet too short
     }
-    crtp_packet_t pkt;
+    
     memcpy(&pkt, data, sizeof(pkt));
 
-    motor_control(pkt.thrust);
-    printf("Motor thrust set to %d\n", pkt.thrust);
+}
 
-    // ESP_LOGI("PARSE_CRTP", "Channel:%x, Port:%x, Roll:%.2f, Pitch:%.2f, Yaw:%.2f, Thrust:%d%%\n", 
-                            // pkt.channel, pkt.port, pkt.roll, -pkt.pitch, pkt.yaw, pkt.thrust * 100 / MAX_THURST_VALUE);
+static void vMotor_control(void *pvParametars)
+{
+    while(1)
+    {
+        parse_crtp_packet(rx_buffer, len);
+        motor_thrust(pkt.thrust, 0);
+        motor_thrust(pkt.thrust, 1);
+        motor_thrust(pkt.thrust, 2);
+        motor_thrust(pkt.thrust, 3);
 
+        printf("Motor thrust set to %d\n", pkt.thrust);
+    }
+
+    vTaskDelete(NULL);
 }
 
 static void vUdp_server(void * pvParametars)
 {
-    uint8_t rx_buffer[1024];
+    
     struct sockaddr_in server_addr, client_addr;
     socklen_t socklen = sizeof(client_addr);
 
@@ -212,18 +240,14 @@ static void vUdp_server(void * pvParametars)
     ESP_LOGI("ESP32_UDP_AP", "UDP server listening on port %d", UDP_PORT);
 
     while (1) {
-        int len = recvfrom(sock, rx_buffer, sizeof(rx_buffer) - 1, 0,
+        len = recvfrom(sock, rx_buffer, sizeof(rx_buffer) - 1, 0,
                            (struct sockaddr *)&client_addr, &socklen);
         if (len < 0) {
             ESP_LOGE("ESP32_UDP_AP", "recvfrom failed: errno %d", errno);
             break;
         } else if (len > 0) {
             rx_buffer[len] = '\0'; // Null-terminate the received data
-            // ESP_LOGI("ESP32_UDP_AP", "Received %d bytes from %s:%d", 
-            //         len, 
-            //         inet_ntoa(client_addr.sin_addr), 
-            //         ntohs(client_addr.sin_port));
-            parse_crtp_packet(rx_buffer, len);
+            // parse_crtp_packet(rx_buffer, len);
         }
     }
 
@@ -246,7 +270,8 @@ void app_main(void)
 
     wifi_init();
 
-    xReturned = xTaskCreate(vTask_mpu6050, "vTask_mpu6050", 4096, NULL, 1, &mpu6050_task_handle);
+    xReturned = xTaskCreate(vTask_mpu6050, "vTask_mpu6050", 4096, NULL, 
+        1, &mpu6050_task_handle);
     if(xReturned != pdPASS)
     {
         printf("Error: Failed to create vTask_mpu6050 task!\n");
@@ -256,6 +281,12 @@ void app_main(void)
     if(xReturned != pdPASS)
     {
         printf("Error: Failed to create  vUdp_server task!\n");
+    }
+
+    xReturned = xTaskCreate(vMotor_control, "vMotor_control", 4096, NULL, 6, NULL);
+    if(xReturned != pdPASS)
+    {
+        printf("Error: Failed to create  vMotor_control task!\n");
     }
 
 
